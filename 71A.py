82ab0376a9b050cf84673eb16904306db7b5612f
abhijitramesh <abhijitramesh2k@gmail.com>
2018-09-24 22:48:06 +0530

    code

---
+++ 71A.py
@@ -0,0 +1,18 @@
+n = int(input())
+i = 0
+for i in range(0,n):
+				i = i+1
+				a = input()
+				if (len(a)>10):
+                    				b = a[0]+str(int(len(a)-2))+a[-1]
+                    				print(b)
+
+				else :
+					print(a)
+
+
+
+                    
+					
+
+
---
+++ watermellon.py
@@ -0,0 +1,8 @@
+w = input()
+if(w == '2'):
+			print("NO")
+else:
+	if(int(w)%2 == 0):
+					print("YES")
+	else:
+		print("NO")
